0c59b167f1f4847c3ac29b3a13721447dd27fc97
zcchen-stu <2724239105@qq.com>
2020-11-08 14:46:26 +0800

    spring IOC 学习，增加子模块

--- src/test/java/com/example/demo/MybatisTest.java
+++ src/test/java/com/example/demo/MybatisTest.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.mybatis.dao.TestEntity;
 import com.example.demo.mybatis.dao.TestMapper;
 import com.example.demo.mybatis.service.impl.MyBatisServiceImpl;
 import org.junit.Test;
@@ -8,14 +9,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DemoApplication.class)
 public class MybatisTest {
     @Autowired
     MyBatisServiceImpl myBatisService;
 
-    @Test
+//    @Test
     public void test() {
         System.out.println(myBatisService.selectById("1"));
+    }
+
+    @Test
+    public void test2() {
+        for(int i = 0; i < 50; i ++ ) {
+            int cur = i % 10;
+            //1900 + year
+            Date date = new Date(120, 11, cur);
+            TestEntity testEntity = new TestEntity("chen" + i, date);
+            myBatisService.add(testEntity);
+            System.out.println(testEntity.getId());
+        }
     }
 }
